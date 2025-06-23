ce9fe5f91d667127a7c590ebe547579dc42fa8d6
mmoengg <mmoengg@gmail.com>
2025-06-23 18:17:25 +0900

    feat: 라우터 폴더 추가, 공통 nav 추가, 메인 화면 이미지 추가

---
+++ src/app/drinks/page.js
@@ -0,0 +1,5 @@
+
+ 
+export default function Drinks() {
+  return <div>Drinks</div>
+}
---
+++ src/components/layout/footer/Footer.jsx
@@ -0,0 +1,14 @@
+
+
+
+
+export default function Footer() {
+    return (
+        <footer className="w-full flex flex-col items-center justify-center p-4 bg-gray-800 text-white">
+            <div className="flex gap-4 items-center flex-col sm:flex-row">
+            </div>
+        </footer>
+            
+
+    )
+}
