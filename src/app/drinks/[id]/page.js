d048233b6ea9f6bd7553f77fa2b0b4964641144b
mmoengg <mmoengg@gmail.com>
2025-06-24 19:02:58 +0900

    style: 태그 리스트 스와이퍼 css 수정, 검색 인풋 화면 구현

---
+++ src/app/drinks/[id]/page.js
@@ -0,0 +1,79 @@
+
+// import { useEffect, useState } from "react";
+import { Navigation, Pagination } from 'swiper/modules';
+// import { supabase } from '@/utils/supabaseClient';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import Link from "next/link";
+
+
+
+
+export default function  DrinksDetailPages() {
+    // const [tags, setTags] = useState([]);
+    // const [error, setError] = useState(null);
+    //
+    // useEffect(() => {
+    //     // 컴포넌트가 처음 마운트될 때 한 번만 실행
+    //     const fetchTags = async () => {
+    //         const { data, error } = await supabase.from('tags').select('*');
+    //         if (error) setError(error.message);
+    //         else setTags(data);
+    //     };
+    //     fetchTags();
+    // }, []);
+    //
+    //
+    // useEffect(() => {
+    //     console.log('tags', tags);
+    // }, [tags]);
+    //
+    // if (error) return <div>에러: {error}</div>;
+
+    return (
+      <section className="w-full h-full min-h-screen flex flex-col items-center justify-start bg-gray-200">
+        <div className="w-full max-w-[1280px] h-full mt-[60px] py-[40px] px-4 flex flex-col items-center justify-start">
+            <div className="w-full h-[130px] mb-8">
+                    <Swiper
+                        modules={[Navigation]}
+                        spaceBetween={30}
+                        slidesPerView={8}
+                        slidesPerGroup={2}
+                        speed={800}
+                        navigation
+                        pagination={{ clickable: true }}
+                        onSwiper={(swiper) => console.log(swiper)}
+                        onSlideChange={() => console.log('slide change')}
+                        className="w-full h-full"
+                    >
+                        {tags.map((tag, idx) => (
+                            <SwiperSlide key={idx}
+                                         className="p-3 border-[1px] rounded-full text-center !flex items-center justify-center cursor-pointer hover:bg-yellow-300 transition">
+                                <Link key={idx} href={`/drinks/${tag.id}`}>
+                                    {tag.name}
+                                </Link>
+                            </SwiperSlide>
+                        ))}
+                    </Swiper>
+                    <input />
+            </div>
+            <div>
+                <button className="min-w-10">검색</button>
+            </div>
+            <ul className="w-full flex flex-wrap justify-center gap-8">
+                {DRINKS_LIST.map((drink) => (
+                    <DrinkCard
+                        key={drink.id}
+                        image={drink.image}
+                        name={drink.name}
+                        tags={drink.tags}
+                        level={drink.level}
+                    />
+                ))}
+            </ul>
+        </div>
+      </section>
+  )
+}
