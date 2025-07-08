07c03614a2f6bdc4c79c5429a37e52972871e0f4
mmoengg <mmoengg@gmail.com>
2025-07-08 12:36:43 +0900

    style: 헤더 네비 반응형 추가

--- src/components/layout/header/Header.jsx
+++ src/components/layout/header/Header.jsx
@@ -1,25 +1,58 @@
+'use client';
+
+import Link from 'next/link';
+
+import { useState } from 'react';
+
 export default function Header() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(prevState => !prevState);
+    }
+
     return (
-        <header className='relative w-full h-[55px] pl-4 text-surface border-b-base fcc'>
-            <a href='/' className='h-full fcc cursor-pointer'>
+        <header className='relative w-full h-[55px] pl-4 text-surface border-b-base fcb'>
+            <Link href='/' className='h-full fcc cursor-pointer'>
                 <img src='/assets/icons/logo-withskey-b.png' alt='Logo' className='h-12 w-12' />
-            </a>
-            <nav className='h-full pl-[55px] fcx flex-1 transition duration-300'>
-                <a href='/article' className='px-6 fcc'>
+            </Link>
+            <nav className='h-full pl-[55px] fcx flex-1 transition duration-300 hidden sm:flex'>
+                <Link href='/article' className='px-6 fcc'>
                     Article
-                </a>
-                <a href='/drinks' className='px-6 fcc'>
+                </Link>
+                <Link href='/drinks' className='px-6 fcc'>
                     Whisky
-                </a>
-                <a href='/board' className='px-6 fcc'>
+                </Link>
+                <Link href='/board' className='px-6 fcc'>
                     Other
-                </a>
+                </Link>
             </nav>
+
             <ul className='h-full flex'>
+                <li className="h-full px-4 border-l-base fcc flex sm:hidden">
+                    <button type="button" className='w-[34px] h-[34px] rounded-full border cursor-pointer' onClick={toggleMenu}>-</button>
+                </li>
                 <li className="h-full px-4 border-l-base fcc">
                     <button type="button" className='w-[34px] h-[34px] rounded-full border'></button>
                 </li>
             </ul>
+
+            {isMenuOpen && (
+                    <nav className="fixed bg-primary w-[100vw] h-[100vh] top-0 left-0 z-[999] flex flex-col justify-center items-center gap-2">
+                        <Link href='/article' className='p-6 fcc' onClick={toggleMenu}>
+                            Article
+                        </Link>
+                        <Link href='/drinks' className='p-6 fcc' onClick={toggleMenu}>
+                            Whisky
+                        </Link>
+                        <Link href='/board' className='p-6 fcc' onClick={toggleMenu}>
+                            Other
+                        </Link>
+                        <buton type="button" class="absolute bottom-1/4 w-[34px] h-[34px] rounded-full border fcc cursor-pointer" onClick={toggleMenu}>X</buton>
+                    </nav>
+                )
+            }
+
         </header>
     );
 }
